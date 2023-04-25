75339ab2b8f1dd7c6aa82028bc0714bef4d925ee
Douglas <douglas.rtoledo2003@gmail.com>
2023-04-25 10:49:16 -0300

    fix: not showing components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Pricing from "./assets/components/organisms/Pricing.jsx"*/
 
 import Pricing from "./assets/components/organisms/Pricing";
 import Service from "./assets/components/organisms/Service";
+import TitleAwards from "./assets/components/organisms/Awards/Awards.jsx";
+import ListAwards from "./assets/components/organisms/Awards/Awards.jsx";
 
-  export default function App(){
-  return <>
-      <Service/>
-      <Pricing/>
-  </> 
+export default function App() {
+  return (
+    <>
+      <Service />
+      <Pricing />
+      <TitleAwards />
+      <ListAwards />
+    </>
+  );
 }
-
- 
