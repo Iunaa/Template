359909b0dbd8e7d39f21163e804953e26a448e30
iuna Leon <yuna.ads@hotmail.com>
2023-04-26 18:38:04 -0300

    add function

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -2,22 +2,25 @@ import logo from "../../../assets/img/logo.png"
 import menu from "../../../assets/img/menu.png"
 
 function Header(){
+    function changeMenu(){
+       const list = ["About", "Services", "Pricing", "Blog"]
+       console.log(list)
+    }
     return(
         
         
-        <div className="header container">
+        <div className="header container" >
             <nav  className="header__nav-container" >
                 <picture className="header__picture">
-                    <img className="header__logo" src={logo}></img>
+                    <img className="header__logo" src={logo} ></img>
                 </picture>
                  
 
               <div className="header__menu-container">
-                <div className="header__menu" onClick="changeMenu(itens)">
-               
+                <div className="header__mobile">
+                 <img className="header__hamburger" src={menu} onClick={changeMenu}></img>
                 </div>
-                
-                <div className="header__list">
+                <div className="header__desktop">
                   
                     <ul id="itens">
                         <li><a>About</a></li>
@@ -30,10 +33,11 @@ function Header(){
                     
                   
                 </div>
-
+            
+             
                 
             </div>
-            <img className="header__hamburger" src={menu} ></img>
+            
                 
             </nav>
         
