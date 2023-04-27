33407e6a62037fbcd5c22cb85d105ae1dc7f989e
Aldiney Moreira <aldiney.moreira@valtech.com>
2023-04-26 22:12:02 -0300

    add: pricing compoent [WIP]

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
+import "./components/main.scss"
 import Header from "./components/organisms/Header/Header.jsx";
 import Service from "./components/organisms/Service_/Service.jsx";
 import Pricing from "./components/organisms/Pricing/Pricing.jsx";
 
 
 
-  export default function App(){
+export default function App(){
   return <>
-       <Header/>
-       <Service/>
-       <Pricing/>
-      
+    <Header/>
+    <Service/>
+    <Pricing />
   </> 
 }
 
--- src/components/organisms/Pricing/Pricing.jsx
+++ src/components/organisms/Pricing/Pricing.jsx
@@ -1,74 +1,45 @@
-//import CardPricing from "../../molecules/CardPricing/CardPricing"
+import CardPricing from "../../molecules/CardPricing/CardPricing"
+import "./pricing.scss"
 
 export const pricingCardsData = [
-
- {
-
-  title: "Free",
-
-subtitle: "Consultation",
-
-paragraph: "Your digital marketing efforts, instead of handling in-house.",
-
-popular: false,
-
- topics: ["enable", "disabled", "disabled"],
-
- },
-
- {
-
-title: "$1500",
-
-subtitle: "Design",
-
- paragraph: "Provide your business with a variety of digital solutions to promote.",
-
-popular: true,
-
- topics: ["enable", "enable", "disabled"],
-
-},
-
-{
-
- title: "$2900",
-
- subtitle: "Design+Code",
-
- paragraph: "Help you hit your marketing goals and grow your business.",
-
- popular: false,
-
-topics: ["enable", "enable", "enable"],
-
- },
-
+  {
+    title: "Free",
+    subtitle: "Consultation",
+    paragraph: "Your digital marketing efforts, instead of handling in-house.",
+    popular: false,
+    topics: ["enable", "disabled", "disabled"],
+  },
+  {
+    title: "$1500",
+    subtitle: "Design",
+    paragraph: "Provide your business with a variety of digital solutions to promote.",
+    popular: true,
+    topics: ["enable", "enable", "disabled"],
+  },
+  {
+    title: "$2900",
+    subtitle: "Design+Code",
+    paragraph: "Help you hit your marketing goals and grow your business.",
+    popular: false,
+    topics: ["enable", "enable", "enable"],
+  }
 ]
 
+export default function Pricing(){
+  return(
+    <>
+      <section className="pricing">
+        <div className="container">
+          <header className="headings--reverse2">
+            <h2 className="title title--2 pricing__title">Explore Pricing Plans</h2>
+            <h3 className="title title--5">Pricing</h3>
+          </header>
 
-
-
-function Pricing(){
-    return(
-        <>
-            <section className="pricing">
-                <div className="container">
-                    <header className="headings--reverse2">
-                        <h2 className="title--2 pricing__title">Check Our Pricing Plans</h2>
-                        <h3 className="title--5">Pricing</h3>
-
-                    </header>
-                    <div id="pricingCards" className="pricing__cards">
-                        {/* <CardPricing
-                            data={pricingCardsData}
-                        // /> */}
-                    </div>
-                </div>
-
-            </section>
-        </>
-    )
+          <div id="pricingCards" className="pricing__cards">
+            <CardPricing data={pricingCardsData}/>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
-
-export default Pricing;
