aea2211384e5fb41c32e94532f3bb858a3e57f66
Douglas <douglas.rtoledo2003@gmail.com>
2023-04-27 10:38:58 -0300

    refacture: delete styles index and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
+import "./components/main.scss";
 import Header from "./components/organisms/Header/Header.jsx";
 import Service from "./components/organisms/Service_/Service.jsx";
 import Pricing from "./components/organisms/Pricing/Pricing.jsx";
 
-
-
-  export default function App(){
-  return <>
-       <Header/>
-       <Service/>
-       <Pricing/>
-      
-  </> 
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Service />
+      <Pricing />
+    </>
+  );
 }
-
- 
