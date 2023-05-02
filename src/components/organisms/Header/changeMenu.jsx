a19818c26bf458a70725c1952f8c5a177fce7a78
iuna Leon <yuna.ads@hotmail.com>
2023-05-02 12:01:54 -0300

    fix

--- src/components/organisms/Header/changeMenu.jsx
+++
@@ -1,9 +0,0 @@
-function changeMenu(){
-    
-        if (itens.style.display == 'block'){
-            itens.style.display = 'none'
-        } else{
-            itens.style.display = 'block'
-        }
-
-}
