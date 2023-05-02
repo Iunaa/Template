a8546e518d8d305ca4296cbe83bf584dbc5f2fc8
Douglas <douglas.rtoledo2003@gmail.com>
2023-05-02 11:42:15 -0300

    feat: add images imports

--- src/components/molecules/AwardsList/AwardsList.jsx
+++ src/components/molecules/AwardsList/AwardsList.jsx
@@ -6,29 +6,34 @@
 //     </>
 //   );
 // }
-// import SOFA from "../../../assets/img/SOFA-awards.svg";
+import SOFA from "../../../assets/img/logo.png";
+// import KeyBoard from "../../../assets/img/logo.png";
+// import WorkMedia from "../../../assets/img/logo.png";
+import Awwwards from "../../../assets/img/logoAwwwards-awards.svg";
+import CSSdesign from "../../../assets/img/logoCSS-awards.svg";
+import TheFWA from "../../../assets/img/logoTheFWA-awards.svg";
 
 const list = [
   {
-    image: "",
+    image: SOFA,
     title: "SOFA",
     date: "25 Oct, 2021",
     company: "Awwwards",
-    imageCompany: "",
+    imageCompany: Awwwards,
   },
   {
     image: "",
     title: "KeyBoard",
     date: "10 Oct, 2021",
     company: "CSS Design Awards",
-    imageCompany: "",
+    imageCompany: CSSdesign,
   },
   {
-    image: "",
+    image: SOFA,
     title: "Word Media",
     date: "7 Oct, 2021",
     company: "The FWA",
-    imageCompany: "",
+    imageCompany: TheFWA,
   },
 ];
 console.log(list);
@@ -37,38 +42,40 @@ export default function AwardsList(props) {
   return (
     <>
       <div className="awards">
-        <h3 className="awards__titlePage">Awards</h3>
-        {list.map((props) => (
-          <section className="awards__container">
-            <div className="awards__itens">
-              <picture className="awards__picture">
-                <img
-                  src={props.image}
-                  alt="image Awards"
-                  className="awards__image"
-                />
-              </picture>
-              <p className="awards__titleItens">{props.title}</p>
-            </div>
-            <p className="awards__date">{props.date}</p>
-            <div className="awards__company">
-              <p className="awards__comapanyName">{props.company}</p>
-              <picture className="awards__pictureCompany">
-                <img
-                  src={props.imageCompany}
-                  alt="logo company"
-                  className="awards__imageCompany"
-                />
-              </picture>
-            </div>
-          </section>
-        ))}
-        <p className="awards__footer">
-          Want more awards?
-          <a href="#" className="awards__footerLink">
-            Explore Now
-          </a>
-        </p>
+        <div className="container">
+          <h3 className="awards__titlePage">Awards</h3>
+          {list.map((props) => (
+            <section className="awards__container">
+              <div className="awards__itens">
+                <picture className="awards__picture">
+                  <img
+                    src={props.image}
+                    alt="image Awards"
+                    className="awards__image"
+                  />
+                </picture>
+                <p className="awards__titleItens">{props.title}</p>
+              </div>
+              <p className="awards__date">{props.date}</p>
+              <div className="awards__company">
+                <p className="awards__comapanyName">{props.company}</p>
+                <picture className="awards__pictureCompany">
+                  <img
+                    src={props.imageCompany}
+                    alt="logo company"
+                    className="awards__imageCompany"
+                  />
+                </picture>
+              </div>
+            </section>
+          ))}
+          <p className="awards__footer">
+            Want more awards?
+            <a href="#" className="awards__footerLink">
+              Explore Now
+            </a>
+          </p>
+        </div>
       </div>
     </>
   );
