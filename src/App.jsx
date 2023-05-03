023cd1d36f46b7ccf48c3e62da24d9598f54de51
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-03 11:22:12 -0300

    refactor: service component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // import "./components/main.scss";
 import "./components/_base/_index.scss"
 import Header from "./components/organisms/Header/Header.jsx";
-import Service from "./components/organisms/Service_/Service.jsx";
+import Service from "./components/organisms/Service/Service.jsx";
 import Pricing from "./components/organisms/Pricing/Pricing.jsx";
 import Webflow from "./components/organisms/Webflow/Webflow.jsx";
 
 export default function App(){
   return <>
-       <Header/>
-       <Webflow/> 
-       {/* <Service/> */}
-       <Pricing/>
+    <Header/>
+    <Webflow/> 
+    <Service/>
+    <Pricing/>
   </> 
 }
