a75046c9e12defec0e45f7ae62b4f53b43bfcf5f
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-03 17:22:58 -0300

    feat: features component created

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Header from "./components/organisms/Header/Header.jsx";
 import Service from "./components/organisms/Service/Service.jsx";
 import Pricing from "./components/organisms/Pricing/Pricing.jsx";
 import Webflow from "./components/organisms/Webflow/Webflow.jsx";
+import Features from "./components/organisms/Features/Features";
 
 export default function App(){
   return <>
     <Header/>
     <Webflow/> 
+    <Features />
     <Service/>
     <Pricing/>
   </> 
---
+++ src/components/organisms/Features/Features.jsx
@@ -0,0 +1,30 @@
+import "./features.scss"
+import development from "../../../assets/img/development.svg"
+
+export default function Features(props) {
+  return (
+    <section className="features container">
+      <div>
+        <header className="headings--reverse3 features__title">
+          <h2 className="title--3 title--3--white">Custom Web Design</h2>
+          <h3 className="title--5">Features</h3>
+        </header>
+
+        <a className="features__anchor text--white" href="#">Read More</a>
+      </div>
+
+      <div className="features__cards">
+        <div className="features__professional">
+          <img className="features__icon" src={development} alt="icon" />
+          <h4 className="title--4 features__topic">Professional</h4>
+          <p className="text features__paragraph">Full service range including technical skills, design.</p>
+        </div>
+
+        <div className="features__accessibility">
+          <h4 className="title--4 title--4--white features__topic">Accessibility</h4>
+          <p className="text--gray features__paragraph">Business understanding, ability to put themselves.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
