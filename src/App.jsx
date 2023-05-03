861624c192e6f58b39b23e7bed57ce3f143d0f22
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-03 19:33:56 -0300

    feat: about-webflow component created

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Service from "./components/organisms/Service/Service.jsx";
 import Pricing from "./components/organisms/Pricing/Pricing.jsx";
 import Webflow from "./components/organisms/Webflow/Webflow.jsx";
 import Features from "./components/organisms/Features/Features";
+import AboutWebflow from "./components/organisms/About/AboutWebflow";
 
 export default function App(){
   return <>
     <Header/>
     <Webflow/> 
     <Features />
+    <AboutWebflow />
     <Service/>
     <Pricing/>
   </> 
