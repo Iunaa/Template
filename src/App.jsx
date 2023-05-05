fe75ec018debd5da5fb9d9e088b14b2f45a4fbe5
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-05 12:38:25 -0300

    feat: add react-router and initial settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-// import "./components/main.scss";
-import "./components/_base/_index.scss"
-import Header from "./components/organisms/Header/Header.jsx";
-import Service from "./components/organisms/Service/Service.jsx";
-import Pricing from "./components/organisms/Pricing/Pricing.jsx";
-import Webflow from "./components/organisms/Webflow/Webflow.jsx";
-import Features from "./components/organisms/Features/Features";
-import AboutWebflow from "./components/organisms/About/AboutWebflow";
-import Contact from "./components/organisms/Contact/Contact.jsx";
-import Footer from "./components/organisms/Footer/Footer.jsx";
+import {  RouterProvider } from 'react-router-dom';
+import router from './Routes';
 
 export default function App(){
-  return <>
-    <Header/>
-    <Webflow/> 
-    <Features />
-    <AboutWebflow />
-    <Service/>
-    <Pricing/>
-    <Contact/>
-    <Footer/>
-  </> 
+  return (
+    <RouterProvider router={router} />
+  )
 }
