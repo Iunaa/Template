707ed709bc48af85e7a9c0b22ed95154f05dfed7
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-08 11:30:03 -0300

    feat: creat counter component

--- src/components/molecules/Counter/Counter.jsx
+++ src/components/molecules/Counter/Counter.jsx
@@ -1,46 +1,21 @@
 import "./counter.scss"
 
-import design from "../../../assets/img/design.svg"
-import development from "../../../assets/img/development.svg"
-import marketing from "../../../assets/img/marketing.svg"
-
-export default function Counter({ experience, members, projects }) {
-  const [ experienceCreate, experienceIcon ] = experience;
-  const [ membersCreate, membersIcon ] = members;
-  const [ projectsCreate, projectsIcon ] = projects;
+export default function Counter(props) {
+  const { data, background } = props;
 
   return (
     <div className="container">
       <div className="counter">
-        {experienceCreate == true && (
-          <div className={`counter__counterItem ${experienceIcon == true ? "counter__counterItem--icon" : ""}`}>
-            {experienceIcon == true && (<img src={design} alt="" />)}
-            <div>
-              <p className="title--3">42%</p>
-              <p>Years of experience</p>
-            </div>
-          </div>
-        )}
 
-        {membersCreate == true && (
-          <div className={`counter__counterItem ${experienceIcon == true ? "counter__counterItem--icon" : ""}`}>
-            {membersIcon == true && (<img src={development} alt="" />)}
+        {data.map((cur) => (
+          <div className={`counter__counterItem ${cur.icon && "counter__counterItem--icon"}`}>
+            {cur.icon && (<img src={cur.icon} className="icon__card" alt={`${cur.text} icon`} />)}
             <div>
-              <p className="title--3">73+</p>
-              <p>Agency members</p>
+              <p className="title--3">{cur.title}</p>
+              <p>{cur.text}</p>
             </div>
           </div>
-        )}
-
-        {projectsCreate == true && (
-          <div className={`counter__counterItem ${experienceIcon == true ? "counter__counterItem--icon" : ""}`}>
-            {projectsIcon == true && (<img src={marketing} alt="" />)}
-            <div>
-              <p className="title--3">5.000</p>
-              <p>Projects complete</p>
-            </div>
-          </div>
-        )}
+        ))}
       </div>
     </div>
   )
