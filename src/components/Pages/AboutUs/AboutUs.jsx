42e93f9828126f2a820e76f2b03a8a5c9c8afa14
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-08 13:01:49 -0300

    feat: background color variant

--- src/components/Pages/AboutUs/AboutUs.jsx
+++ src/components/Pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,25 @@
+import Counter from "../../molecules/Counter/Counter"
+
 export default function AboutUs() {
-  return <>
-    
-  </>
+  return <div className="">
+    <Counter
+      data={[
+        {
+          title: "42%",
+          text: "Years of experience",
+          // icon: design 
+        },
+        {
+          title: "73+",
+          text: "Agency members",
+          // icon: development 
+        },
+        {
+          title: "5.000",
+          text: "Projects complete",
+          // icon: marketing  
+        }
+      ]}
+    />
+  </div>
 }
--- src/components/molecules/Counter/Counter.jsx
+++ src/components/molecules/Counter/Counter.jsx
@@ -1,21 +1,23 @@
 import "./counter.scss"
 
 export default function Counter(props) {
-  const { data, background } = props;
+  const { data, background="secondary" } = props;
 
   return (
-    <div className="container">
-      <div className="counter">
+    <div className={`counter__background--${background}`}>
+      <div className="container">
+        <div className="counter">
 
-        {data.map((cur) => (
-          <div className={`counter__counterItem ${cur.icon && "counter__counterItem--icon"}`}>
-            {cur.icon && (<img src={cur.icon} className="icon__card" alt={`${cur.text} icon`} />)}
-            <div>
-              <p className="title--3">{cur.title}</p>
-              <p>{cur.text}</p>
+          {data.map((cur) => (
+            <div className={`counter__counterItem ${cur.icon && "counter__counterItem--icon"}`}>
+              {cur.icon && (<img src={cur.icon} className="icon__card" alt={`${cur.text} icon`} />)}
+              <div>
+                <p className="title--3">{cur.title}</p>
+                <p>{cur.text}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
