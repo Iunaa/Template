40574da19913a0a8b4e0de6e4468ce5e0a688bef
Douglas <douglas.rtoledo2003@gmail.com>
2023-05-09 03:09:17 -0300

    feat: creation of the footer portifio and addition of social image

--- src/components/Pages/Portifolio/Portifolio.jsx
+++ src/components/Pages/Portifolio/Portifolio.jsx
@@ -1,6 +1,8 @@
 import Header from "../../organisms/Header/Header";
 import PortifolioBanner from "../../molecules/PortifolioBanner/PortifolioBanner";
 import PortifolioCard from "../../atoms/PortifolioCard/portifolioCard";
+import PortifolioFooter from "../../molecules/PortifolioFooter/PortifolioFooter";
+import Footer from "../../organisms/Footer/Footer";
 
 import Logo from "../../../assets/img/logoAgency.svg";
 export default function Portifolio() {
@@ -9,6 +11,8 @@ export default function Portifolio() {
       <Header logoimg={Logo} />
       <PortifolioBanner />
       <PortifolioCard />
+      <PortifolioFooter />
+      <Footer />
     </>
   );
 }
