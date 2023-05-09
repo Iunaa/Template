da2e9d09678ee097a312ebe1a736f63f48615a3e
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-09 11:36:24 -0300

    refactor: adjust spacing

--- src/components/molecules/Counter/Counter.jsx
+++ src/components/molecules/Counter/Counter.jsx
@@ -2,16 +2,18 @@ import "./counter.scss"
 
 export default function Counter(props) {
   const { data, background="secondary" } = props;
+  let lengthData = `--${data.length}`
 
   return (
     <div className={`counter__background--${background}`}>
       <div className="container">
         <div className="counter">
+          {console.log(data)}
 
           {data.map((cur) => (
-            <div className={`counter__counterItem ${cur.icon && "counter__counterItem--icon"}`}>
+            <div className={`counter__counterItem${lengthData} ${cur.icon && "counter__counterItem--icon"}`}>
               {cur.icon && (<img src={cur.icon.src} className="icon__card" alt={cur.icon.alt} />)}
-              <div>
+              <div className="counter__text">
                 <p className="title--3">{cur.title}</p>
                 <p>{cur.text}</p>
               </div>
