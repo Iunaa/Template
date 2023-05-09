239990f831adee23dc02bea8823effa9cda20a18
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-09 16:34:49 -0300

    feat: add hero to about and home page

--- src/components/Pages/AboutUs/AboutUs.jsx
+++ src/components/Pages/AboutUs/AboutUs.jsx
@@ -1,39 +1,32 @@
 import Counter from "../../molecules/Counter/Counter"
 import Header from "../../organisms/Header/Header"
 
-import design from "../../../assets/img/design.svg"
-import development from "../../../assets/img/development.svg"
-import marketing from "../../../assets/img/marketing.svg"
 import logoHeader from "../../../assets/img/logoAgency.svg"
+import Hero from "../../molecules/Hero/Hero"
 
 export default function AboutUs() {
   return <>
     <Header logoImg={logoHeader} />
+    <Hero
+        text={{
+          title:"About Us",
+          paragraph:"Agency provides a full service range including technical skills, design, business understanding.",
+        }}
+        style={"center"}
+      />
     <Counter
       data={[
         {
           title: "42%",
           text: "Years of experience",
-          // icon: {
-          //   alt: "",
-          //   src: design
-          // },
         },
         {
           title: "73+",
           text: "Agency members",
-          // icon: {
-          //   alt: "",
-          //   src: development
-          // },
         },
         {
           title: "5.000",
           text: "Projects complete",
-          // icon: {
-          //   alt: "",
-          //   src: marketing
-          // },  
         }
       ]}
     />
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,22 +1,42 @@
 // import "./components/main.scss";
 import "../../../components/_base/_index.scss"
+import logoWebflow from "../../../assets/img/logoWebflow.svg"
+import bannerHero from "../../../assets/img/bannerHero.svg"
+
 
 import Header from "../../../components/organisms/Header/Header.jsx";
 import Service from "../../../components/organisms/Service/Service.jsx";
 import Pricing from "../../../components/organisms/Pricing/Pricing.jsx";
-import Webflow from "../../../components/organisms/Webflow/Webflow.jsx";
+// import Webflow from "../../../components/organisms/Webflow/Webflow.jsx";
 import Features from "../../../components/organisms/Features/Features";
 import AboutWebflow from "../../../components/organisms/About/AboutWebflow";
 import Contact from "../../../components/organisms/Contact/Contact.jsx";
 import Footer from "../../../components/organisms/Footer/Footer.jsx";
-
-import logoWebflow from "../../../assets/img/logoWebflow.svg"
+import Hero from "../../molecules/Hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Header logoImg={logoWebflow} />
-      <Webflow/> 
+      <Hero
+        img={bannerHero}
+        text={{
+          tag:"morden studio",
+          title:"We’re Help To Build Your Dream Project",
+          paragraph:"Agency provides a full service range including technical skills, design, business understanding.",
+          button: [
+            {
+              text: "How We Work",
+              style: "button button--secondary",
+            },
+            {
+              text: "Contact Us",
+              style: "button",
+            },
+          ]
+        }}
+      />
+      {/* <Webflow/>  */}
       <Features />
       <AboutWebflow />
       <Service/>
