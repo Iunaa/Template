e4440366be1b5dfef317994ef7ebd3aa31195a91
Douglas <douglas.rtoledo2003@gmail.com>
2023-05-09 05:08:44 -0300

    fix: page break fix

--- src/components/molecules/PortifolioFilter/PortifolioFilter.jsx
+++ src/components/molecules/PortifolioFilter/PortifolioFilter.jsx
@@ -5,94 +5,51 @@ export default function PortifolioFilter() {
   const [busca, setBusca] = useState("");
   return (
     <>
-      <div className="portifolio-filterMobile">
-        <div className="portifolio-filterMobile__container">
+      <div className="portifolio-filter">
+        <div className="portifolio-filter__container">
           <select
             name="selecao"
             id="filter"
-            className="portifolio-filterMobile__selecao"
+            className="portifolio-filter__selecao"
             value={busca}
             onChange={(ev) => setBusca(ev.target.value)}
           >
             <option
               value="showAll"
               name="showall"
-              className="portifolio-filterMobile__itens"
+              className="portifolio-filter__itens"
             >
               Show All
             </option>
             <option
               value="design"
               name="design"
-              className="portifolio-filterMobile__itens"
+              className="portifolio-filter__itens"
             >
               Design
             </option>
             <option
               value="branding"
               name="branding"
-              className="portifolio-filterMobile__itens"
+              className="portifolio-filter__itens"
             >
               Branding
             </option>
             <option
               value="illustration"
               name="illustration"
-              className="portifolio-filterMobile__itens"
+              className="portifolio-filter__itens"
             >
               Illustration
             </option>
             <option
               value="motion"
               name="motion"
-              className="portifolio-filterMobile__itens"
+              className="portifolio-filter__itens"
             >
               Motion
             </option>
           </select>
-        </div>
-      </div>
-
-      <div className="portifolio-filterDesktop">
-        <div>
-          <p className="portifolio-filterDesktop__text">
-            <a href="#" className="portifolio-filterDesktop__link">
-              Show All
-            </a>
-          </p>
-          <p className="portifolio-filterDesktop__number">14</p>
-        </div>
-        <div>
-          <p className="portifolio-filterDesktop__text">
-            <a href="#" className="portifolio-filterDesktop__link">
-              Design
-            </a>
-          </p>
-          <p className="portifolio-filterDesktop__number">6</p>
-        </div>
-        <div>
-          <p className="portifolio-filterDesktop__text">
-            <a href="#" className="portifolio-filterDesktop__link">
-              Branding
-            </a>
-          </p>
-          <p className="portifolio-filterDesktop__number">4</p>
-        </div>
-        <div>
-          <p className="portifolio-filterDesktop__text">
-            <a href="#" className="portifolio-filterDesktop__link">
-              Illustration
-            </a>
-          </p>
-          <p className="portifolio-filterDesktop__number">3</p>
-        </div>
-        <div>
-          <p className="portifolio-filterDesktop__text">
-            <a href="#" className="portifolio-filterDesktop__link">
-              Motion
-            </a>
-          </p>
-          <p className="portifolio-filterDesktop__number">1</p>
         </div>
       </div>
     </>
