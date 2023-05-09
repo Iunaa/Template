333aff218c54cf0893d71077c78dba0bc99719f7
Douglas <douglas.rtoledo2003@gmail.com>
2023-05-09 00:31:03 -0300

    feat: create card mobile

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,17 +1,22 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
 
-import Home from '../components/Pages/Home/Home';
-import AboutUs from '../components/Pages/AboutUs/AboutUs';
+import Home from "../components/Pages/Home/Home";
+import AboutUs from "../components/Pages/AboutUs/AboutUs";
+import Portifolio from "../components/Pages/Portifolio/Portifolio";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />
-	},
   {
-		path: "/about",
-		element: <AboutUs />
-	},
-])
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <AboutUs />,
+  },
+  {
+    path: "/portifolio",
+    element: <Portifolio />,
+  },
+]);
 
-export default router
+export default router;
--- src/components/atoms/PortifolioCard/PortifolioCard.jsx
+++ src/components/atoms/PortifolioCard/PortifolioCard.jsx
@@ -3,6 +3,7 @@ import KeyBoard from "../../../assets/img/KeyBoard.svg";
 import DDDone from "../../../assets/img/DDDone.svg";
 import WorkMedia from "../../../assets/img/WordMedia.svg";
 import HandP from "../../../assets/img/HandP.svg";
+import "./portifolioCard.scss";
 
 const list = [
   {
@@ -36,15 +37,25 @@ console.log(list);
 export default function PortifolioCard(props) {
   return (
     <>
-      {list.map((props) => (
-        <div>
-          <picture>
-            <img src={props.img} alt="image card" />
-          </picture>
-          <a href="#">{props.button}</a>
-          <h2>{props.title}</h2>
-        </div>
-      ))}
+      <div className="portifolio-cards">
+        {list.map((props) => (
+          <section className="portifolio-cards__itens">
+            <picture className="portifolio-cards__picture">
+              <img
+                src={props.img}
+                alt="image card"
+                className="portifolio-cards__image"
+              />
+            </picture>
+            <div className="portifolio-cards__text">
+              <a href="#" className="portifolio-cards__button">
+                {props.button}
+              </a>
+              <h2 className="portifolio-cards__title">{props.title}</h2>
+            </div>
+          </section>
+        ))}
+      </div>
     </>
   );
 }
