80609a6e4491e02eeceb5688f44e7dec4cc833ef
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-10 10:31:03 -0300

    feat: add component on about page

--- src/components/Pages/AboutUs/AboutUs.jsx
+++ src/components/Pages/AboutUs/AboutUs.jsx
@@ -3,17 +3,32 @@ import Header from "../../organisms/Header/Header"
 
 import logoHeader from "../../../assets/img/logoAgency.svg"
 import Hero from "../../molecules/Hero/Hero"
+import Features from "../../organisms/Features/Features"
 
 export default function AboutUs() {
   return <>
     <Header logoImg={logoHeader} />
     <Hero
-        text={{
-          title:"About Us",
-          paragraph:"Agency provides a full service range including technical skills, design, business understanding.",
-        }}
-        style={"center"}
-      />
+      text={{
+        title:"About Us",
+        paragraph:"Agency provides a full service range including technical skills, design, business understanding.",
+      }}
+      style={"center"}
+    />
+    <Features 
+      text={{
+        paragraph: "Long run, and work as an extension of the merchant's team.",
+        description: "Ability to put themselves in the merchant's shoes. It is meant to partner on the long run, and work as an extension. ",
+        // title: "Custom Web Design",
+        // link: {
+        //   // style: "text--white", 
+        //   src: "#",
+        //   content: "Read More",
+        // }
+      }}
+
+      cardStyle="secondary "
+    />
     <Counter
       data={[
         {
