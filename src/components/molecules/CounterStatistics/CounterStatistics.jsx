b91c7b5f7f92ff66748541a0fe4834f35a061c74
Aldiney Moreira <alldiney.mr@gmail.com>
2023-05-15 15:23:09 -0300

    refactor: improving border logic between elements

--- src/components/molecules/CounterStatistics/CounterStatistics.jsx
+++ src/components/molecules/CounterStatistics/CounterStatistics.jsx
@@ -2,19 +2,17 @@ import "./counterStatistics.scss"
 
 export default function CounterStatistics(props) {
   const { data, background="secondary" } = props;
-  let lengthData = `--${data.length}`
 
   return (
     <div className={`counter__background--${background}`}>
       <div className="container">
         <div className="counter">
-
           {data.map((cur, idx) => (
-            <div key={idx} className={`counter__counterItem${lengthData} ${cur.icon && "counter__counterItem--icon"}`}>
+            <div key={idx} className={`counter__item${cur.icon ? " counter__item--icon": ""} ${`counter__item--${cur.background}`}`}>
               {cur.icon && (<img src={cur.icon.src} className="icon__card" alt={cur.icon.alt} />)}
               <div className="counter__text">
                 <p className="text--h3">{cur.title}</p>
-                <p className="text--brown-dark">{cur.text}</p>
+                <p className="text--big text--brown-dark">{cur.text}</p>
               </div>
             </div>
           ))}
