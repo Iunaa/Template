51c8a81bf831190e35e1e2f879a11344f78e29ef
yuna.ads@hotmail.com <yuna.ads@hotmail.com>
2023-10-30 16:27:47 -0300

    fix

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,13 +1,12 @@
 import Logo from "../../atoms/Logo/Logo"
 import Hamburger from "../../molecules/Hamburger/Hamburger"
 import Navbar from "../../molecules/Navbar/Navbar"
-
-import "./header.scss"
+import Styles from "./header.scss"
 
 export default function Header({ logoimg }){
     return (
-        <header className="header">
-            <nav className="navbar container">
+        <header className={Styles.header}>
+            <nav className={`${Styles.navbar} container`}>
                 <Logo img={logoimg} />
                 <Hamburger />
                 <Navbar />
